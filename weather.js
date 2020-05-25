edc8107bdceadb1830c218c62f0df7d6d612c02d
amit verma <amitverma@Rajeevs-MacBook-Pro.local>
2020-05-25 17:07:23 +0300

    added start command in package.json

--- weather.js
+++ weather.js
@@ -2,10 +2,10 @@ const axios = require('axios')
 
 
 const MAPQUEST_URL = 'http://www.mapquestapi.com/geocoding/v1/address';
-const MAPQUEST_API_KEY = 'WGA0EzzO3cJyUUPSKrpufn3NRYghzRBb';
+const MAPQUEST_API_KEY = process.env.MAPQUEST_API_KEY;
 
 const DARKSKY_URL = 'https://api.darksky.net/forecast/';
-const DARKSKY_API_KEY = 'a3a4aae6287d4d587dc89954ed49ebac';
+const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
 
 //let address = '15 A2 Piilipuuntie 02250'
 
@@ -19,7 +19,7 @@ const getWeather = async (address) => {
             }
         });
 
-        console.log(geo_response)
+        //console.log(geo_response)
 
         if (geo_response.data.info.statuscode != 0) {
             throw geo_response.data.info.messages
@@ -29,17 +29,17 @@ const getWeather = async (address) => {
             lat = geo_response.data.results[0].locations[0].latLng.lat;
             lng = geo_response.data.results[0].locations[0].latLng.lng;
 
-            console.log({ lat, lng })
+            //console.log({ lat, lng })
 
             darkSkyURL = DARKSKY_URL + DARKSKY_API_KEY + '/' + lat + ',' + lng
 
-            console.log(darkSkyURL)
+            //console.log(darkSkyURL)
 
             weather_response = await axios.get(darkSkyURL, {
                 params: { units: 'ca' }
             });
-            console.log(weather_response.data.currently.temperature)
-            console.log(weather_response.data.hourly.summary)
+            //console.log(weather_response.data.currently.temperature)
+            //console.log(weather_response.data.hourly.summary)
 
             return {
                 temprature: weather_response.data.currently.temperature,
